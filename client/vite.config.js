f85fde295a73bde2ea81d42c8fee6ef12c0d351b
Abhishek Raj <abhishekcultivates@gmail.com>
2024-11-15 21:37:28 +0530

    updated vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: "./client/postcss.config.js", // Ensure the PostCSS config path is correct
+  },
   build: {
     outDir: "client/dist", // Specify the build output directory
   },
